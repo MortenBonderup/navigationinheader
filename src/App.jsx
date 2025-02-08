685d014aaf9398bf90f04f84db82351edc5aa5e7
Morten Bonderup <moab@eaaa.dk>
2025-02-08 12:45:23 +0100

    Version5

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./Layout";
-import FirstPage from "./pages/FirstPage";
-import SecondPage from "./pages/SecondPage";
-import NotFound from "./pages/NotFound";
+import FirstPage from "./views/FirstPage";
+import SecondPage from "./views/SecondPage";
+import NotFound from "./views/NotFound";
 import "./App.css";
-import DefaultPage from "./pages/DefaultPage";
+import DefaultPage from "./views/DefaultPage";
 
 const router = createBrowserRouter([
   {
